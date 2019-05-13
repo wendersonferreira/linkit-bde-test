22825109af6090e2aeb3ebe88e095d5b2b465bcc
Wenderson Fereira de Souza <wenderson@trustsystems.com.br>
2019-05-13 20:27:25 +0100

    #2: base code

--- build.sbt
+++ build.sbt
@@ -2,6 +2,14 @@ name := "linkit-bde-test"
 
 version := "0.1.0-ALPHA1-SNAPSHOT"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.11.8"
+
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.2" % "provided"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs@_*) => MergeStrategy.discard
+  case _ => MergeStrategy.first
+}
--- src/main/scala/nl/linkit/bde/ApplicationEntryPoint.scala
+++ src/main/scala/nl/linkit/bde/ApplicationEntryPoint.scala
@@ -1,5 +1,47 @@
 package nl.linkit.bde
 
+import java.io.File
+
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
 object ApplicationEntryPoint extends App {
+
+  val sparkSession = SparkSession.builder().appName("LinkitBDE")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val basePathString = "/root/linkit/data-spark"
+
+  def getListOfFile(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists() && d.isDirectory) {
+      d.listFiles.filter(_.isFile).filter(_.getName.endsWith(".csv")).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def listOfDataFrame(targets: List[File]): Map[String, DataFrame] = {
+    targets.map(file => (file.getName.substring(0,file.getName.lastIndexOf(".")), sparkSession.read.format("csv").option("header", "true").option("inferSchema", "true").csv("file:///" + file.getAbsolutePath))).toMap
+  }
+
+  val dfs = listOfDataFrame(getListOfFile(basePathString))
+
+  def sanitizeColumns(t: DataFrame): DataFrame = {
+    t.select(t.columns.map {
+      c => t.col(c).as(if (c.contains("-")) c.replace("-", "_") else c)
+    }: _*)
+  }
+
+  dfs.foreach(f => {
+    sanitizeColumns(f._2).write.format("ORC").mode(SaveMode.Append).saveAsTable("linkit." + f._1)
+  })
+
+
+  sparkSession.sql("use linkit")
+
+  val driverInfo = sparkSession.sql("SELECT a.driverid, a.name, t.hours_logged, t.miles_logged FROM drivers a LEFT JOIN timesheet t ON a.driverid=t.driverid")
+
+  driverInfo.show(5)
 
 }
